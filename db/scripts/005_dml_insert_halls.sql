b93cf33be528f8654949e3d7f155bd983b15ca9f
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2024-05-06 19:15:29 +0300

    Заполнение таблиц halls и film_sessions

---
+++ db/scripts/005_dml_insert_halls.sql
@@ -0,0 +1,8 @@
+insert into halls(name, row_count, place_count, description) values('№1', 15, 10, 'Стандартные кинозалы с возможностью демонстрации фильмов в 2D и 3D форматах.');
+insert into halls(name, row_count, place_count, description) values('№2', 15, 10, 'Стандартные кинозалы с возможностью демонстрации фильмов в 2D и 3D форматах.');
+insert into halls(name, row_count, place_count, description) values('№3', 15, 10, 'Стандартные кинозалы с возможностью демонстрации фильмов в 2D и 3D форматах.');
+insert into halls(name, row_count, place_count, description) values('№4', 12, 8, 'Стандартные кинозалы с возможностью демонстрации фильмов в 2D и 3D форматах.');
+insert into halls(name, row_count, place_count, description) values('№5', 12, 8, 'Стандартные кинозалы с возможностью демонстрации фильмов в 2D и 3D форматах.');
+insert into halls(name, row_count, place_count, description) values('№6', 18, 12, 'Стандартные кинозалы с возможностью демонстрации фильмов в 2D и 3D форматах.');
+insert into halls(name, row_count, place_count, description) values('IMAX №1', 8, 16, 'Кинозалы, оборудованные технологией кинопоказа IMAX, которая полностью погружает зрителя в происходящее на экране.');
+insert into halls(name, row_count, place_count, description) values('IMAX №2', 8, 16, 'Кинозалы, оборудованные технологией кинопоказа IMAX, которая полностью погружает зрителя в происходящее на экране.');

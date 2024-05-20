943be6327a01a44404165e0a5b6923ffa1eef58a
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2024-05-20 21:47:56 +0300

    fix модели Film

--- src/main/java/ru/job4j/cinema/model/Film.java
+++ src/main/java/ru/job4j/cinema/model/Film.java
@@ -1,14 +1,25 @@
 package ru.job4j.cinema.model;
 
 import lombok.*;
+import java.util.Map;
 import java.util.Objects;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Film {
+
+    public static final Map<String, String> COLUMN_MAPPING = Map.of(
+            "id", "id",
+            "name", "name",
+            "description", "description",
+            "year", "year",
+            "genre_id", "genreId",
+            "minimal_age", "minimalAge",
+            "duration_in_minutes", "durationInMinutes",
+            "file_id", "fileId"
+    );
 
     private int id;
 
@@ -18,13 +29,13 @@ public class Film {
 
     private int year;
 
-    private Genre genre;
+    private int genreId;
 
     private int minimalAge;
 
     private int durationInMinutes;
 
-    private File file;
+    private int fileId;
 
     @Override
     public boolean equals(Object o) {

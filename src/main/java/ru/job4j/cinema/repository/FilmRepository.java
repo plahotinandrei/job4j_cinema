27a8c29d6d660c0169e1656fd7dbdb605d611c22
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2024-05-20 21:46:15 +0300

    fix FilmRepository

--- src/main/java/ru/job4j/cinema/repository/FilmRepository.java
+++ src/main/java/ru/job4j/cinema/repository/FilmRepository.java
@@ -2,8 +2,11 @@ package ru.job4j.cinema.repository;
 
 import ru.job4j.cinema.model.Film;
 import java.util.Collection;
+import java.util.Optional;
 
 public interface FilmRepository {
+
+    Optional<Film> findById(int id);
 
     Collection<Film> findAll();
 }
